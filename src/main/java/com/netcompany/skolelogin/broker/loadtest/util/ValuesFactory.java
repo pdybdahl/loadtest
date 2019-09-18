2ff5bc116abd5dbf38f0f4d2a79d4c9916addd99
Paw Dybdahl <pdyb@netcompany.com>
2019-09-18 11:22:50 +0200

    Rette fejl i filreference

--- src/main/java/com/netcompany/skolelogin/broker/loadtest/util/ValuesFactory.java
+++ src/main/java/com/netcompany/skolelogin/broker/loadtest/util/ValuesFactory.java
@@ -6,56 +6,54 @@ import java.util.List;
 
 public class ValuesFactory {
 
-    public static List<String> genererMuligeInstitutionsnumre() throws IOException {
-        return readFile("c:\\Projects\\Loadtest\\Resources\\Institutionsnumre.csv");
+  public static List<String> genererMuligeInstitutionsnumre() throws IOException {
+    return readFile("Institutionsnumre.csv");
+  }
+
+  public static List<String> genererMuligeDrengenavne() throws IOException {
+    return readFile("Alle godkendte drengenavne per 2019-09-06.csv");
+  }
+
+  public static List<String> genererMuligePigenavne() throws IOException {
+    return readFile("Alle godkendte pigenavne per 2019-09-06.csv");
+  }
+
+  public static List<String> genererMuligeEfternavne() throws IOException {
+    return readFile("Frie efternavne.txt");
+  }
+
+  private static List<String> readFile(String fileName) throws IOException {
+    BufferedReader buffer = new BufferedReader(new InputStreamReader(ValuesFactory.class.getClassLoader().getResourceAsStream(fileName)));
+
+    List<String> allNames = new ArrayList<>();
+    String name = null;
+    while ((name = buffer.readLine()) != null) {
+      if (isAllLetters(name)) {
+        allNames.add(name);
+      }
     }
 
-    public static List<String> genererMuligeDrengenavne() throws IOException {
-        return readFile("c:\\Projects\\Loadtest\\Resources\\Alle godkendte drengenavne per 2019-09-06.csv");
+    return allNames;
+  }
+
+  private static boolean isAllLetters(String str) {
+    char[] chars = str.toCharArray();
+
+    for (int i = 0; i < chars.length; i++) {
+      char c = str.toCharArray()[i];
+      if (!Character.isLetter(c) && !Character.isDigit(c)) {
+        return false;
+      }
     }
 
-    public static List<String> genererMuligePigenavne() throws IOException {
-        return readFile("c:\\Projects\\Loadtest\\Resources\\Alle godkendte pigenavne per 2019-09-06.csv");
+    return true;
+  }
+
+  public static void main(String[] args) {
+    try {
+      genererMuligePigenavne().stream().forEach(l -> System.out.println(l));
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-
-    public static List<String> genererMuligeEfternavne() throws IOException {
-        return readFile("c:\\Projects\\Loadtest\\Resources\\Frie efternavne.txt");
-    }
-
-    private static List<String> readFile(String absoluteFileName) throws IOException {
-        File file = new File(absoluteFileName);
-        FileReader fileReader = new FileReader(file);
-        BufferedReader buffer = new BufferedReader(fileReader);
-
-        List<String> allNames = new ArrayList<>();
-        String name = null;
-        while ((name = buffer.readLine()) != null) {
-            if (isAllLetters(name)) {
-                allNames.add(name);
-            }
-        }
-
-        return allNames;
-    }
-
-    private static boolean isAllLetters(String str) {
-        char[] chars = str.toCharArray();
-
-        for (int i = 0; i < chars.length; i++) {
-            char c = str.toCharArray()[i];
-            if (!Character.isLetter(c) && !Character.isDigit(c)) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    public static void main(String[] args) {
-        try {
-            genererMuligePigenavne().stream().forEach(l -> System.out.println(l));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+  }
 }
